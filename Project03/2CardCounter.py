26e503cf8695365cebe2993ef48b71d9ee79074b
Daniel Valverde <valverdedaniel44@gmail.com>
2021-02-18 08:18:27 -0700

    almost done P3

--- Project03/2CardCounter.py
+++ Project03/2CardCounter.py
@@ -1,4 +1,3 @@
-
 ###Background Information
 #The counting system works by keeping a running total in your head (hot Count) that tracks the ratio between high and low cards.
 # the counting system will have a positive running caount when the deck has more high cards than low cards.
@@ -14,8 +13,6 @@
 #Cards between 10-Ace are -1
 #for example at the beginning of the game your hot Count will start at 0 but as soon as you draw one card lets say a King
 ## then  your hot Count will be -1 ; lets say you draw a Queen next your hot Count will be -2 you keep going until you run out of cards or mess up
-
-
 
 # alias for shuffle as shuffle
 from random import shuffle as shuffle
@@ -35,20 +32,8 @@ class Cards:
         shuffle(self.deck)
 
     def drawCard(self):
-        ups = ['2', '3', '4', '5', '6']
-        downs = ['10', 'Jack', 'Queen', 'King', 'Ace']
         self.hands = [self.deck.pop()]
-        # self.hands = self.deck.pop()
-#         print('from withindrawcard',self.hands)
-        #
-        # if any(x in self.hands for x in downs):
-        #     print('the deck is getting colder')
-        #     self.hotCount -= 1
-        # if any(y in self.hands for y in ups):
-        #     print('the deck is getting hotter')
-        #     self.hotCount += 1
-            #
-#         print('from withindrawcard', self.hotCount)
+
 
 #instantiating cards
 c = Cards()
@@ -63,34 +48,34 @@ def game():
         c.drawCard()
         print('')
         print('The current card is: ', c.hands)
+        #if the card is 10-A hotCount -1
         if any(x in str(c.hands) for x in downs):
 #             print('the deck is getting colder')
             c.hotCount -= 1
+        #if the card is 2-6 hotCount +1
         if any(y in str(c.hands) for y in ups):
 #             print('the deck is getting hotter')
             c.hotCount += 1
-        #TODO comment out the answer to hotCount
+        #we will tell you the answer for the first couple of cards so you learn how to play
         if len(c.deck) > 49:
             print('the current hot count is: ', c.hotCount)
         print('Type in a number for the exact Hot Count I hope you are keeping track')
         a = input()
         try:
-            # a = int(a)
+            a = int(a)
             # if you set a = c.hotcount you can type any number and simulate what it feels like to win!
-            a = c.hotCount
+            # a = c.hotCount
             if a == c.hotCount:
+                #recursively calling game to keep playing
                 game()
             else:
                 print('wrong count!')
                 print('Ending Hot Count: ', c.hotCount)
-
         except:
             print('Ending Hot Count: ', c.hotCount)
             print('you chose poorly!')
 
-
 game()
-
 
 print('wow you made it', 52-len(c.deck), 'cards deep')
 if len(c.deck) <= 0:
